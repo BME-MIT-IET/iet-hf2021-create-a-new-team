7b93a9584d643b1508e0e2c7af5610bccbf508a3
MexPain <61620062+MexPain@users.noreply.github.com>
2021-05-16 01:28:40 +0200

    RDFOntologyFact és Literal tesztek

---
+++ RDFSharp.UnitTests/RDFOntologyLiteralTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RDFSharp.Model;
+using RDFSharp.Semantics.OWL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDFSharp.UnitTests
+{
+    [TestClass]
+    public class RDFOntologyLiteralTest
+    {
+
+        [TestMethod]
+        public void CreateOntologyLiteral_Success()
+        {
+            //Arrange
+            var literal = new RDFPlainLiteral("Garfield");
+
+            //Act
+            var ontologyLiteral = new RDFOntologyLiteral(literal);
+            
+            //Assert
+            Assert.AreEqual(ontologyLiteral.Value, literal);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(RDFSemanticsException))]
+        public void CreateOntologyLiteral_Failure()
+        {
+            //Arrange
+            RDFPlainLiteral literal = null;
+
+            //Act
+            var ontologyLiteral = new RDFOntologyLiteral(literal);
+
+
+        }
+    }
+}
